03d302f2b45e19925236816cc07899f51da690b6
Johannes Asfaha <johannesasfaha@johanness-mbp-2.home>
2018-11-11 12:22:48 +0000

    intial xcode project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Johannes Asfaha on 11/11/2018.
+//  Copyright © 2018 Johannes Asfaha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
